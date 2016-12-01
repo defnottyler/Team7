ddcdbf70236ebda26d0974668b8e6bf48f9bf623
tycanady <tycanady@gmail.com>
2016-12-01 16:50:23 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
-#include <iostream>
 #include "Board.h"
 #include "Cards.h"
 
-using namespace std;
+int main() {
 
-int main()
-{
+	Board *board = new Board();
+	
+	int p1Score = 0;
+	int p2Score = 0;
+
+	do {
+		board->playRound(&p1Score, &p2Score);
+	} while (p1Score != 2 || p2Score != 2);
+
 	return 0;
 }
