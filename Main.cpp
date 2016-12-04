655f0c8261db6687f8ad79a6f61b6a1491ff6205
josephbozarth <joseph.bozarth@ufl.edu>
2016-12-04 12:22:54 -0500

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,10 @@
 using namespace std;
 
 int main()
-{
+{	
+	string yesOrNo = "j";
+	bool play = true;
+	while(play){
 	cout << "\033[2J\033[1;1H"; //Magic terminal eraser
 	Board *myBoard = new Board();
 	
@@ -20,6 +23,41 @@ int main()
 		cout << p2Score <<endl;
 		
 	} while (p1Score != 2 && p2Score != 2);
+	
+	if (p1Score > p2Score)
+	{
+		cout <<"Player One is the winner!!" << endl;
+	}
+	else if (p1Score < p2Score)
+	{
+		cout <<"Player Two is the winner!!" << endl;
+	}
+	else
+	{
+		cout <<"The game is a draw."<<endl;
+	}
+	cout << "Play again?\n" << "y/n" << endl;
+	
+	cin >> yesOrNo;
+	//yesOrNo = toLowerCase(yesOrNo));
+	
+	/*while (yesOrNo != 'y' || yesOrNo != 'n' || cin.fail() )
+	{
+		cout << yesOrNo << "\n";
+		cout << "please enter y for yes or n for no" << endl;
+		cin.clear();
+		cin.ignore(256,'\n');
+		cin >> yesOrNo;
+		putchar (tolower(yesOrNo));
+	} */
+	if (yesOrNo == "n" || yesOrNo == "N")
+		{
+			play = false;
+		}
+	
+	
+}
+	
 	
 		
 	return 0;
